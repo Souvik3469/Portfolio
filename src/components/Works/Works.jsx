6e389f0f5aa0ea2d0bbd33036f02571715255180
Souvik <souvik3469@gmail.com>
2022-06-20 20:49:13 +0530

    first commit

---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,83 @@
+import React, { useContext } from "react";
+import "./Works.css";
+import Upwork from "../../img/Upwork.png";
+import Fiverr from "../../img/fiverr.png";
+import Amazon from "../../img/amazon.png";
+import Shopify from "../../img/Shopify.png";
+import Facebook from "../../img/Facebook.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import {Link} from 'react-scroll'
+const Works = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  return (
+    <div className="works" id="works">
+      {/* left side */}
+      <div className="w-left">
+        <div className="awesome">
+          {/* dark Mode */}
+          <span style={{ color: darkMode ? "white" : "" }}>
+            About Me
+          </span>
+          <span>
+          <spane>
+            Currently I am a Cse undergrad at Techno Main Salt Lake trying to
+            <br />
+            make some kind progress in both professional and personal life
+            
+            <br />
+            and constantly trying to upskill myself as per the industry.
+            <br />
+            Apart from that I pass my leisure time sketching,watching anime and little 
+            <br />
+            bit of workout to stay fit.
+          </spane>
+          </span>
+          <Link to="contact" smooth={true} spy={true}>
+            <button className="button s-button">Hire Me</button>
+          </Link>
+          <div
+            className="blur s-blur1"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+
+        {/* right side */}
+      </div>
+      <div className="w-right">
+        <motion.div
+          initial={{ rotate: 45 }}
+          whileInView={{ rotate: 0 }}
+          viewport={{ margin: "-40px" }}
+          transition={{ duration: 3.5, type: "spring" }}
+          className="w-mainCircle"
+        >
+          <div className="w-secCircle">
+            <img src={Upwork} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Fiverr} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Amazon} alt="" />
+          </div>{" "}
+          <div className="w-secCircle">
+            <img src={Shopify} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Facebook} alt="" />
+          </div>
+        </motion.div>
+        {/* background Circles */}
+        <div className="w-backCircle blueCircle"></div>
+        <div className="w-backCircle yellowCircle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
